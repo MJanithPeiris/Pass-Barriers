ad3060668da5f98cfcaa9f435057b16b8f9218a7
Jason Mathew <jasonmathewfd@gmail.com>
2022-04-04 13:12:53 +0530

    version 1.0.3

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,9 +4,21 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public float forwardForce = 4000f;
+    public float sidewayForce = 1000f;
+
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(0, 0, 4000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
     }
 }
