baec35285c3f927f4996580f2d5be8f3999dbd99
samadhi0 <92273380+samadhi0@users.noreply.github.com>
2022-04-12 16:14:41 +0530

    Add EndTrigger Script

--- Assets/Scripts/EndTrigger.cs
+++ Assets/Scripts/EndTrigger.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EndTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+     public GameManager gameManager;
+   void OnTriggerEnter()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gameManager.CompleteLevel();
     }
 }
