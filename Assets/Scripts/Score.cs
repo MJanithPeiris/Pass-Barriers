044ac5224517684feda37bcc421cf5755babf3f2
Yevzzz <ybogahawatte2003@gmail.com>
2022-04-12 13:46:04 +0530

    Version 1.0.4 Add Score Script

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform coin;
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = coin.position.z.ToString("0");
+    }
+}
