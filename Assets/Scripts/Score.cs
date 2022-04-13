30ae56facd3ffe192e98abc2ba410033039d6b14
samadhi0 <92273380+samadhi0@users.noreply.github.com>
2022-04-13 10:57:22 +0530

    Update Score Script

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -3,12 +3,13 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    public Transform coin;
-    public Text scoreText;
+    public Transform player;
+public Text scoreText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //scoreText.text = coin.position.z.ToString("0");
-    }
+// Update is called once per frame
+void Update()
+{
+//scoreText.text = player.position.z.ToString("0");
+scoreText.text = GameManager.numberofcoins.ToString("0");
+}
 }
